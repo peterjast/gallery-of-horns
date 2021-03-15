7aedef61569e9a1d80c9d1745bbb2b5f85fe8cb7
Peter Staker <peterjast@gmail.com>
2021-03-15 16:14:21 -0700

    lab01

---
+++ src/beasts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+  render() {
+    return (
+      <div id="beasts">
+        <h2>{this.props.title}</h2>
+        <img src={this.props.image_url} alt={this.props.description} title={this.props.title}></img>
+        <p>{this.props.description}</p>
+      </div>
+    );
+  }
+}
+
+export default HornedBeasts;
