12a04e1ae4095804920d92cac031c351a27a4311
Peter Staker <peterjast@gmail.com>
2021-03-15 17:29:22 -0700

    fixing eslint config issue

--- src/beasts.js
+++ src/beasts.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class HornedBeasts extends React.Component {
   render() {
-    return (
+    return(
       <div id="beasts">
         <h2>{this.props.title}</h2>
         <img src={this.props.image_url} alt={this.props.description} title={this.props.title}></img>
